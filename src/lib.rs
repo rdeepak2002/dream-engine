384093aa00fe298edb81eb077af10e0a5e47be7d
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-10 20:36:02 -0500

    Depth texture broken when resizing but everything else works

--- src/lib.rs
+++ src/lib.rs
@@ -43,7 +43,8 @@ struct State {
     demo_app: egui_demo_lib::DemoWindows,
     #[allow(dead_code)]
     diffuse_texture: texture::Texture,
-    depth_texture: texture::Texture,
+    depth_texture_1: texture::Texture,
+    depth_texture_2: texture::Texture,
     frame_texture: texture::Texture,
     frame_texture_view: Option<wgpu::TextureView>,
 }
@@ -136,8 +137,11 @@ impl State {
             source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
         });
 
-        let depth_texture =
-            texture::Texture::create_depth_texture(&device, &config, "depth_texture");
+        let depth_texture_1 =
+            texture::Texture::create_depth_texture(&device, &config, "depth_texture_1");
+
+        let depth_texture_2 =
+            texture::Texture::create_depth_texture(&device, &config, "depth_texture_2");
 
         let frame_texture =
             texture::Texture::create_frame_texture(&device, &config, "frame_texture");
@@ -217,7 +221,8 @@ impl State {
             egui_winit_context,
             demo_app,
             diffuse_texture,
-            depth_texture,
+            depth_texture_1,
+            depth_texture_2,
             frame_texture,
             frame_texture_view: None,
         }
@@ -233,28 +238,81 @@ impl State {
             self.config.width = new_size.width;
             self.config.height = new_size.height;
             self.surface.configure(&self.device, &self.config);
-            self.depth_texture =
-                texture::Texture::create_depth_texture(&self.device, &self.config, "depth_texture");
+            self.depth_texture_1 = texture::Texture::create_depth_texture(
+                &self.device,
+                &self.config,
+                "depth_texture_1",
+            );
+            self.depth_texture_2 = texture::Texture::create_depth_texture(
+                &self.device,
+                &self.config,
+                "depth_texture_2",
+            );
         }
     }
 
     fn update(&mut self) {}
+
+    fn render1(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let texture_view = self
+            .frame_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder 1"),
+            });
+
+        // draw triangle
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass 1"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &texture_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                    view: &self.depth_texture_1.view,
+                    depth_ops: Some(wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(1.0),
+                        store: true,
+                    }),
+                    stencil_ops: None,
+                }),
+            });
+
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+
+        let output_texture_view = self
+            .frame_texture
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        self.frame_texture_view = Some(output_texture_view);
+
+        Ok(())
+    }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
         let view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
-
-        let mut new_frame_texture_view: Option<wgpu::TextureView> = None;
-
-        if self.frame_texture_view.is_none() {
-            new_frame_texture_view = Some(
-                self.frame_texture
-                    .texture
-                    .create_view(&wgpu::TextureViewDescriptor::default()),
-            );
-        }
 
         {
             // define rendering of egui elements
@@ -329,11 +387,6 @@ impl State {
                     ui.vertical_centered(|ui| {
                         ui.label("TODO: scene hierarchy");
                     });
-
-                    // TODO: render result onto image using this
-                    // ui.image();
-
-                    // ui.separator();
                 });
 
             egui::TopBottomPanel::top("render-controls")
@@ -374,7 +427,7 @@ impl State {
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
+                label: Some("Render Encoder 2"),
             });
 
         {
@@ -396,50 +449,10 @@ impl State {
                 &egui_screen_descriptor,
             );
 
-            // draw to another texture
-            {
-                if new_frame_texture_view.is_some() {
-                    let texture_view = new_frame_texture_view.unwrap();
-                    let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                        label: Some("Render Pass"),
-                        color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                            view: &texture_view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(wgpu::Color {
-                                    r: 0.1,
-                                    g: 0.2,
-                                    b: 0.3,
-                                    a: 1.0,
-                                }),
-                                store: true,
-                            },
-                        })],
-                        depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                            view: &self.depth_texture.view,
-                            depth_ops: Some(wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(1.0),
-                                store: true,
-                            }),
-                            stencil_ops: None,
-                        }),
-                    });
-
-                    render_pass.set_pipeline(&self.render_pipeline);
-                    render_pass.draw(0..3, 0..1);
-
-                    // self.egui_wgpu_renderer.render(
-                    //     &mut render_pass,
-                    //     &paint_jobs,
-                    //     &screen_descriptor,
-                    // );
-                }
-            }
-
-            // draw triangle
+            // draw editor
             {
                 let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                    label: Some("Render Pass"),
+                    label: Some("Render Pass 2"),
                     color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                         view: &view,
                         resolve_target: None,
@@ -454,7 +467,7 @@ impl State {
                         },
                     })],
                     depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                        view: &self.depth_texture.view,
+                        view: &self.depth_texture_2.view,
                         depth_ops: Some(wgpu::Operations {
                             load: wgpu::LoadOp::Clear(1.0),
                             store: true,
@@ -462,9 +475,6 @@ impl State {
                         stencil_ops: None,
                     }),
                 });
-
-                render_pass.set_pipeline(&self.render_pipeline);
-                render_pass.draw(0..3, 0..1);
 
                 self.egui_wgpu_renderer.render(
                     &mut render_pass,
@@ -476,13 +486,6 @@ impl State {
             self.queue.submit(iter::once(encoder.finish()));
             output.present();
         }
-
-        let output_texture_view = self
-            .frame_texture
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-
-        self.frame_texture_view = Some(output_texture_view);
 
         for id in &egui_full_output.textures_delta.free {
             self.egui_wgpu_renderer.free_texture(id);
@@ -580,11 +583,11 @@ pub async fn run() {
         {
             unsafe {
                 if NEED_TO_RESIZE_WINDOW {
-                    state
-                        .window()
-                        .set_inner_size(winit::dpi::PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
-                    state.resize(winit::dpi::PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
-                    NEED_TO_RESIZE_WINDOW = false;
+                    // state
+                    //     .window()
+                    //     .set_inner_size(winit::dpi::PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
+                    // state.resize(winit::dpi::PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
+                    // NEED_TO_RESIZE_WINDOW = false;
                 }
             }
         }
@@ -609,6 +612,18 @@ pub async fn run() {
             }
             Event::RedrawRequested(window_id) if window_id == state.window().id() => {
                 state.update();
+
+                match state.render1() {
+                    Ok(_) => {}
+                    // Reconfigure the surface if it's lost or outdated
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        state.resize(state.size)
+                    }
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                    // We're ignoring timeouts
+                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+                }
 
                 match state.render() {
                     Ok(_) => {}
