3e597864170be993311c7c2651bad6fc513acf79
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-15 22:25:27 -0400

    Window resize handler is now done entirely in Rust code using winit, closure, and control beam
    Took 1 hour 12 minutes

--- src/lib.rs
+++ src/lib.rs
@@ -16,38 +16,12 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  **********************************************************************************/
 
-use dream_editor;
-use dream_renderer;
+mod app;
+mod window;
 
-use winit::{
-    event::*,
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
-
+use crate::window::Window;
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
-
-cfg_if::cfg_if! {
-    if #[cfg(target_arch = "wasm32")] {
-        static mut WINDOW_WIDTH: u32 = 2000;
-        static mut WINDOW_HEIGHT: u32 = 1500;
-        static mut NEED_TO_RESIZE_WINDOW: bool = false;
-    }
-}
-
-cfg_if::cfg_if! {
-    if #[cfg(target_arch = "wasm32")] {
-        #[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-        pub fn resize_window(width: u32, height: u32) {
-            unsafe {
-                WINDOW_WIDTH = width;
-                WINDOW_HEIGHT = height;
-                NEED_TO_RESIZE_WINDOW = true;
-            }
-        }
-    }
-}
 
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
@@ -60,126 +34,6 @@ pub async fn run() {
         }
     }
 
-    {
-        // example execution of javascript code
-        // let js_code = "console.log('hello world');";
-        let js_code = "7 * 8.1";
-        let mut context = boa_engine::Context::default();
-        match context.eval(js_code) {
-            Ok(res) => {
-                println!("{}", res.to_string(&mut context).unwrap());
-                log::warn!("{}", res.to_string(&mut context).unwrap());
-            }
-            Err(e) => {
-                // Pretty print the error
-                eprintln!("Uncaught {}", e.display());
-                log::error!("Uncaught {}", e.display());
-            }
-        };
-    }
-
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(winit::dpi::PhysicalSize::new(3000, 1750))
-        .build(&event_loop)
-        .unwrap();
-
-    cfg_if::cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-        } else {
-            window.set_title("Dream Engine");
-        }
-    }
-
-    #[cfg(target_arch = "wasm32")]
-    {
-        // Winit prevents sizing with CSS, so we have to set
-        // the size manually when on web.
-        window.set_inner_size(winit::dpi::PhysicalSize::new(2000, 1500));
-
-        use winit::platform::web::WindowExtWebSys;
-        web_sys::window()
-            .and_then(|win| win.document())
-            .and_then(|doc| {
-                let dst = doc.get_element_by_id("wasm-example")?;
-                let canvas = web_sys::Element::from(window.canvas());
-                dst.append_child(&canvas).ok()?;
-                Some(())
-            })
-            .expect("Couldn't append canvas to document body.");
-    }
-
-    let mut renderer = dream_renderer::RendererWgpu::new(&window).await;
-    let mut editor =
-        dream_editor::EditorEguiWgpu::new(&renderer, window.scale_factor() as f32, &event_loop)
-            .await;
-    event_loop.run(move |event, _, control_flow| {
-        #[cfg(target_arch = "wasm32")]
-        {
-            unsafe {
-                if NEED_TO_RESIZE_WINDOW {
-                    window
-                        .set_inner_size(winit::dpi::PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
-                    renderer.resize(winit::dpi::PhysicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT));
-                    editor.handle_resize(&renderer);
-                    NEED_TO_RESIZE_WINDOW = false;
-                }
-            }
-        }
-        match event {
-            Event::RedrawRequested(window_id) if window_id == window.id() => {
-                match renderer.render() {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        renderer.resize(renderer.size);
-                        editor.handle_resize(&renderer);
-                    }
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // We're ignoring timeouts
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-                }
-
-                match editor.render_wgpu(&renderer, &window) {
-                    Ok(_) => {}
-                    // Reconfigure the surface if it's lost or outdated
-                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                        renderer.resize(renderer.size);
-                        editor.handle_resize(&renderer);
-                    }
-                    // The system is out of memory, we should probably quit
-                    Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
-                    // We're ignoring timeouts
-                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
-                }
-
-                // set aspect ratio of camera for renderer after rendering editor and knowing the size of the center panel
-                renderer.set_camera_aspect_ratio(editor.renderer_aspect_ratio);
-            }
-            Event::WindowEvent { event, .. } => {
-                if !editor.handle_event(&event, &renderer) {
-                    match event {
-                        WindowEvent::Resized(physical_size) => {
-                            renderer.resize(physical_size);
-                            editor.handle_resize(&renderer);
-                        }
-                        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                            // new_inner_size is &mut so w have to dereference it twice
-                            renderer.resize(*new_inner_size);
-                            editor.handle_resize(&renderer);
-                        }
-                        _ => (),
-                    }
-                }
-            }
-            Event::MainEventsCleared => {
-                // RedrawRequested will only trigger once, unless we manually
-                // request it.
-                window.request_redraw();
-            }
-            _ => {}
-        }
-    });
+    let window = Window::new();
+    window.start_loop().await;
 }
---
+++ src/window.rs
@@ -0,0 +1,146 @@
+use crate::app::App;
+use crossbeam_channel::unbounded;
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub struct Window {
+    pub window: winit::window::Window,
+    pub event_loop: EventLoop<()>,
+}
+
+impl Window {
+    pub fn new() -> Self {
+        let event_loop = EventLoop::new();
+        let window = WindowBuilder::new()
+            .with_inner_size(winit::dpi::PhysicalSize::new(3000, 1750))
+            .build(&event_loop)
+            .unwrap();
+
+        cfg_if::cfg_if! {
+            if #[cfg(target_arch = "wasm32")] {
+            } else {
+                window.set_title("Dream Engine");
+            }
+        }
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            let win = web_sys::window().unwrap();
+
+            window.set_inner_size(winit::dpi::LogicalSize::new(
+                win.inner_width().unwrap().as_f64().unwrap() as f32,
+                win.inner_height().unwrap().as_f64().unwrap() as f32,
+            ));
+
+            use winit::platform::web::WindowExtWebSys;
+            web_sys::window()
+                .and_then(|win| win.document())
+                .and_then(|doc| {
+                    let dst = doc.get_element_by_id("wasm-example")?;
+                    let canvas = web_sys::Element::from(window.canvas());
+                    dst.append_child(&canvas).ok()?;
+                    Some(())
+                })
+                .expect("Couldn't append canvas to document body.");
+        }
+
+        Self { window, event_loop }
+    }
+
+    pub async fn start_loop(self) {
+        // listen for screen resizing events for web build
+        let (tx, rx) = unbounded::<winit::dpi::LogicalSize<f32>>();
+
+        #[cfg(target_arch = "wasm32")]
+        {
+            use wasm_bindgen::JsCast;
+            let closure = wasm_bindgen::closure::Closure::wrap(Box::new(move |e: web_sys::Event| {
+                let win = web_sys::window().unwrap();
+                let new_window_size = winit::dpi::LogicalSize::new(
+                    win.inner_width().unwrap().as_f64().unwrap() as f32,
+                    win.inner_height().unwrap().as_f64().unwrap() as f32,
+                );
+                tx.send(new_window_size).unwrap();
+            }) as Box<dyn FnMut(_)>);
+            let window = web_sys::window().unwrap();
+            window
+                .add_event_listener_with_callback("resize", closure.as_ref().unchecked_ref())
+                .unwrap();
+            closure.forget();
+        }
+
+        let mut renderer = dream_renderer::RendererWgpu::new(&self.window).await;
+        let mut editor = dream_editor::EditorEguiWgpu::new(
+            &renderer,
+            self.window.scale_factor() as f32,
+            &self.event_loop,
+        )
+        .await;
+        let app: App = App::new();
+
+        self.event_loop.run(move |event, _, control_flow| {
+            match event {
+                Event::RedrawRequested(window_id) if window_id == self.window.id() => {
+                    if let Some(size) = rx.clone().try_iter().last() {
+                        self.window.set_inner_size(size);
+                    }
+
+                    app.update();
+
+                    match renderer.render() {
+                        Ok(_) => {}
+                        // Reconfigure the surface if it's lost or outdated
+                        Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                            renderer.resize(renderer.size);
+                            editor.handle_resize(&renderer);
+                        }
+                        // The system is out of memory, we should probably quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                        // We're ignoring timeouts
+                        Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+                    }
+
+                    match editor.render_wgpu(&renderer, &self.window) {
+                        Ok(_) => {}
+                        // Reconfigure the surface if it's lost or outdated
+                        Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                            renderer.resize(renderer.size);
+                            editor.handle_resize(&renderer);
+                        }
+                        // The system is out of memory, we should probably quit
+                        Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                        // We're ignoring timeouts
+                        Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+                    }
+
+                    // set aspect ratio of camera for renderer after rendering editor and knowing the size of the center panel
+                    renderer.set_camera_aspect_ratio(editor.renderer_aspect_ratio);
+                }
+                Event::WindowEvent { event, .. } => {
+                    if !editor.handle_event(&event, &renderer) {
+                        match event {
+                            WindowEvent::Resized(physical_size) => {
+                                renderer.resize(physical_size);
+                                editor.handle_resize(&renderer);
+                            }
+                            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                                // new_inner_size is &mut so w have to dereference it twice
+                                renderer.resize(*new_inner_size);
+                                editor.handle_resize(&renderer);
+                            }
+                            _ => (),
+                        }
+                    }
+                }
+                Event::MainEventsCleared => {
+                    self.window.request_redraw();
+                }
+                _ => {}
+            }
+        });
+    }
+}
