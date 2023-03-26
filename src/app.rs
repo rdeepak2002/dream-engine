36ebc441333ffacf3c99f693c04fb19ccef5535a
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-25 22:32:18 -0400

    Add commented out code for generating wgpu mesh from glb data
    Took 7 minutes

--- src/app.rs
+++ src/app.rs
@@ -87,20 +87,67 @@ impl App {
                         primitives.for_each(|primitive| {
                             let reader =
                                 primitive.reader(|buffer| Some(&buffer_data[buffer.index()]));
-
-                            if let Some(vertex_attibute) = reader.read_positions().map(|v| {
-                                dbg!(v);
-                            }) {
-                                // Save the position here using mapped vertex_attribute result
+                            let mut vertices = Vec::new();
+                            if let Some(vertex_attribute) = reader.read_positions() {
+                                vertex_attribute.for_each(|vertex| {
+                                    dbg!(vertex);
+                                    vertices.push(dream_renderer::model::ModelVertex {
+                                        position: vertex,
+                                        tex_coords: Default::default(),
+                                        normal: Default::default(),
+                                    })
+                                });
                             }
-                            // let material = primitive.material().index();
-                            // let indices = primitive.indices();
-                            // let reader = primitive.reader(|buffer| Some(&buffer_data[buffer.index()]));
-                            // if let Some(vertex_attibute) = reader.read_positions().map(|v| {
-                            //     dbg!(v);
-                            // }) {
-                            //     // Save the position here using mapped vertex_attribute result
-                            // }
+                            if let Some(normal_attribute) = reader.read_normals() {
+                                let mut normal_index = 0;
+                                normal_attribute.for_each(|normal| {
+                                    dbg!(normal);
+                                    vertices[normal_index].normal = normal;
+
+                                    normal_index += 1;
+                                });
+                            }
+                            if let Some(tex_coord_attribute) =
+                                reader.read_tex_coords(0).map(|v| v.into_f32())
+                            {
+                                let mut tex_coord_index = 0;
+                                tex_coord_attribute.for_each(|tex_coord| {
+                                    dbg!(tex_coord);
+                                    vertices[tex_coord_index].tex_coords = tex_coord;
+
+                                    tex_coord_index += 1;
+                                });
+                            }
+
+                            let mut indices = Vec::new();
+                            if let Some(indices_raw) = reader.read_indices() {
+                                // dbg!(indices_raw);
+                                indices.append(&mut indices_raw.into_u32().collect::<Vec<u32>>());
+                            }
+                            dbg!(indices);
+
+                            // let mesh_name = mesh.name().expect("No mesh name found");
+                            // let vertex_buffer =
+                            //     device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                            //         label: Some(&format!("{} {:?} Vertex Buffer", path, mesh_name)),
+                            //         contents: bytemuck::cast_slice(&vertices),
+                            //         usage: wgpu::BufferUsages::VERTEX,
+                            //     });
+                            // let index_buffer =
+                            //     device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                            //         label: Some(&format!("{} {:?} Index Buffer", path, mesh_name)),
+                            //         contents: bytemuck::cast_slice(&indices),
+                            //         usage: wgpu::BufferUsages::INDEX,
+                            //     });
+                            //
+                            // meshes.push(dream_renderer::model::Mesh {
+                            //     name: mesh_name.to_string(),
+                            //     vertex_buffer,
+                            //     index_buffer,
+                            //     num_elements: indices.len() as u32,
+                            //     // material: m.mesh.material_id.unwrap_or(0),
+                            //     material: 0,
+                            // });
                         });
                     }
                 }
