b9e4999fe4c2690498b4aa7fae92ee524e52032a
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-03-25 23:44:38 -0400

    Add test models for testing later
    Took 6 minutes

--- dream-renderer/src/lib.rs
+++ dream-renderer/src/lib.rs
@@ -424,8 +424,10 @@ impl RendererWgpu {
         //     usage: wgpu::BufferUsages::INDEX,
         // });
 
-        // let mesh_list = gltf_loader::read_gltf("ice_cube.glb", &device).await;
         let mesh_list = gltf_loader::read_gltf("cube.glb", &device).await;
+        // let mesh_list = gltf_loader::read_gltf("Box.glb", &device).await;
+        // let mesh_list = gltf_loader::read_gltf("ice_cube.glb", &device).await;
+        // let mesh_list = gltf_loader::read_gltf("cube_sketchfab.glb", &device).await;
         // TODO: do something with this cube mesh
 
         // let index_buffer = cube_mesh.index_buffer;
