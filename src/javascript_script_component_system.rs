301187ad2d85fb4e9e2dfc85839ef6addaee228c
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-04-07 15:30:38 -0400

    Update TODO
    Took 38 seconds

--- src/javascript_script_component_system.rs
+++ src/javascript_script_component_system.rs
@@ -42,9 +42,6 @@ impl ComponentSystem for JavaScriptScriptComponentSystem {
             let js_code: String = include_str!("../res/script.js").into();
             let mut context = boa_engine::Context::default();
 
-            // TODO: for persistence instead of saving context we should save a JS value? not sure
-            // ^ this would involve storing the resulting compiled javascript class object which seems fine imo
-
             // evaluate all code (expects a javascript class object to be returned at the end)
             match context.eval(js_code) {
                 Ok(res) => {
@@ -53,6 +50,7 @@ impl ComponentSystem for JavaScriptScriptComponentSystem {
                     context.register_global_class::<Vector3JS>().unwrap();
                     // get script class returned and call its update method
                     let js_obj = res.as_object().expect("No object returned by script");
+                    // TODO: for persistence store this js object (probs better than storing context)
                     let js_obj_update_func = js_obj
                         .get("update", &mut context)
                         .expect("No update function found");
