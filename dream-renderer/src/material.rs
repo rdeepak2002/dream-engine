a716aed0910f2ddb3df23b7c0cf91c05faa57f0b
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-04-30 23:10:28 -0400

    Simplfiy layout by taking away unnecessary paddings
    Took 10 minutes

--- dream-renderer/src/material.rs
+++ dream-renderer/src/material.rs
@@ -6,15 +6,11 @@ pub struct MaterialFactors {
     pub base_color: [f32; 3],
     pub _padding1: f32,
     pub emissive: [f32; 3],
+    pub _padding2: f32,
     pub metallic: f32,
     pub roughness: f32,
     pub alpha: f32,
     pub alpha_cutoff: f32,
-    pub _padding2: f32,
-    pub _padding3: f32,
-    pub _padding4: f32,
-    pub _padding5: f32,
-    pub _padding6: f32,
 }
 
 impl MaterialFactors {
@@ -34,10 +30,10 @@ impl MaterialFactors {
             alpha: *(base_color.get(3).unwrap_or(&1.0)),
             alpha_cutoff,
             _padding2: 0.,
-            _padding3: 0.,
-            _padding4: 0.,
-            _padding5: 0.,
-            _padding6: 0.,
+            // _padding3: 0.,
+            // _padding4: 0.,
+            // _padding5: 0.,
+            // _padding6: 0.,
         }
     }
 }
