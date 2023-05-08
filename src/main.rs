c82349370c807e5c461b6f3a4c1038aae03dddc8
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-05-08 11:30:35 -0700

    Remove more unused dependencies
    Took 6 minutes

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,6 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  **********************************************************************************/
 
-use dream;
-
 fn main() {
     pollster::block_on(dream::run());
 }
