8e01ff27dae1a679a8055061c7e3c10a3f008af2
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-01 00:22:58 -0700

    Update rust python system to use block expression compiler mode
    Took 1 hour 57 minutes

---
+++ crates/dream-app/src/default-files/script.py
@@ -0,0 +1,9 @@
+class Foo:
+    def __init__(self):
+        pass
+
+    def func(self):
+        return 5
+
+
+Foo()
--- crates/dream-app/src/python_script_component_system.rs
+++ crates/dream-app/src/python_script_component_system.rs
@@ -1,7 +1,14 @@
 use std::sync::{Mutex, Weak};
 
-use rustpython_vm as vm;
+use gc::{Finalize, Trace};
+use rustpython_vm::convert::{ToPyObject, ToPyResult};
+use rustpython_vm::function::{FuncArgs, IntoPyNativeFunc};
+use rustpython_vm::{
+    compiler, pyclass, pymodule, Interpreter, PyObject, PyObjectRef, PyPayload, PyResult,
+    TryFromBorrowedObject, VirtualMachine,
+};
 
+use dream_ecs::component::Transform;
 // use dream_ecs::component::Transform;
 // use dream_ecs::entity::Entity;
 use dream_ecs::scene::Scene;
@@ -14,45 +21,122 @@ pub struct PythonScriptComponentSystem {
 
 impl Default for PythonScriptComponentSystem {
     fn default() -> Self {
-        Self {
-            interpreter: vm::Interpreter::without_stdlib(Default::default()),
-        }
+        let interpreter = rustpython_vm::Interpreter::with_init(Default::default(), |vm| {
+            // vm.add_native_modules(rustpython_stdlib::get_module_inits());
+            // vm.add_native_module("rust_py_module".to_owned(), Box::new(make_module));
+        });
+        // let interpreter = Interpreter::without_stdlib(Default::default());
+        Self { interpreter }
     }
 }
 
 impl System for PythonScriptComponentSystem {
-    fn update(&mut self, _dt: f32, _scene: Weak<Mutex<Scene>>) {
-        // let transform_entities = scene
-        //     .upgrade()
-        //     .expect("Unable to upgrade")
-        //     .lock()
-        //     .expect("Unable to lock")
-        //     .get_entities_with_component::<Transform>();
-        // for entity_id in transform_entities {
-        //     let _entity = Entity::from_handle(entity_id, scene);
-        //     #[allow(clippy::needless_late_init)]
-        //     self.interpreter.enter(|vm| {
-        //         let scope = vm.new_scope_with_builtins();
-        //         let source_path;
-        //         cfg_if::cfg_if! {
-        //             if #[cfg(target_arch = "wasm32")] {
-        //                 source_path = "<wasm>"
-        //             } else {
-        //                 source_path = "<embedded>"
-        //             }
-        //         }
-        //         let code_obj = vm
-        //             .compile(r#"5"#, vm::compiler::Mode::Eval, source_path.to_owned())
-        //             .map_err(|err| vm.new_syntax_error(&err))
-        //             .unwrap();
-        //         let py_obj_ref = vm
-        //             .run_code_obj(code_obj, scope)
-        //             .expect("Error running python code");
-        //         let _res = py_obj_ref
-        //             .try_int(vm)
-        //             .expect("Error getting python result")
-        //             .to_string();
-        //     })
-        // }
+    fn update(&mut self, _dt: f32, scene: Weak<Mutex<Scene>>) {
+        let transform_entities = scene
+            .upgrade()
+            .expect("Unable to upgrade")
+            .lock()
+            .expect("Unable to lock")
+            .get_entities_with_component::<Transform>();
+        for entity_id in transform_entities {
+            let script = include_str!("default-files/script.py");
+            // let _entity = Entity::from_handle(entity_id, scene);
+            self.interpreter.enter(|vm| {
+                let scope = vm.new_scope_with_builtins();
+                let source_path;
+                cfg_if::cfg_if! {
+                    if #[cfg(target_arch = "wasm32")] {
+                        source_path = "<wasm>"
+                    } else {
+                        source_path = "<embedded>"
+                    }
+                }
+                let code_obj = vm
+                    .compile(script, compiler::Mode::BlockExpr, source_path.to_owned())
+                    .map_err(|err| vm.new_syntax_error(&err))
+                    .unwrap();
+                vm.run_code_obj(code_obj, scope)
+                    .map(|value| {
+                        let func = value.get_attr("func", vm).unwrap();
+                        let res = vm
+                            .invoke(&func, ())
+                            .unwrap()
+                            .try_int(vm)
+                            .unwrap()
+                            .to_string();
+                        println!("{res}");
+                        log::warn!("{res}");
+                    })
+                    .expect("Error running python code");
+            })
+        }
     }
 }
+
+// #[pymodule]
+// mod rust_py_module {
+//     use super::*;
+//     use rustpython_vm::{
+//         builtins::PyList, convert::ToPyObject, PyObjectRef, TryFromBorrowedObject,
+//     };
+//
+//     #[pyfunction]
+//     fn rust_function(
+//         num: i32,
+//         s: String,
+//         python_person: PythonEntity,
+//         _vm: &VirtualMachine,
+//     ) -> PyResult<RustStruct> {
+//         println!(
+//             "Calling standalone rust function from python passing args:
+//             num: {},
+//             string: {},
+//             python_person.handle: {}",
+//             num, s, python_person.handle
+//         );
+//         Ok(RustStruct {
+//             numbers: NumVec(vec![1, 2, 3, 4]),
+//         })
+//     }
+//
+//     #[derive(Debug, Clone)]
+//     struct NumVec(Vec<i32>);
+//
+//     impl ToPyObject for NumVec {
+//         fn to_pyobject(self, vm: &VirtualMachine) -> PyObjectRef {
+//             let list = self.0.into_iter().map(|e| vm.new_pyobj(e)).collect();
+//             PyList::new_ref(list, vm.as_ref()).to_pyobject(vm)
+//         }
+//     }
+//
+//     #[pyattr]
+//     #[pyclass(module = "rust_py_module", name = "RustStruct")]
+//     #[derive(Debug, PyPayload)]
+//     struct RustStruct {
+//         numbers: NumVec,
+//     }
+//
+//     #[pyclass]
+//     impl RustStruct {
+//         #[pygetset]
+//         fn numbers(&self) -> NumVec {
+//             self.numbers.clone()
+//         }
+//
+//         #[pymethod]
+//         fn print_in_rust_from_python(&self) {
+//             println!("Calling a rust method from python");
+//         }
+//     }
+//
+//     struct PythonEntity {
+//         handle: u64,
+//     }
+//
+//     impl TryFromBorrowedObject for PythonEntity {
+//         fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
+//             let handle = obj.get_attr("handle", vm)?.try_into_value::<u64>(vm)?;
+//             Ok(PythonEntity { handle })
+//         }
+//     }
+// }
