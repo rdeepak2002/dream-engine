a2af22ea184b7830d2067ce4d9f7f2bb4240e679
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-04 10:03:42 -0700

    Example of setting position attribute of transform from Python scripting side
    Took 39 minutes

--- crates/dream-app/src/default-files/script.py
+++ crates/dream-app/src/default-files/script.py
@@ -2,10 +2,12 @@ from dream import get_entity
 
 
 class PythonScript:
-    def update(self, handle):
+    def update(self, handle: int):
         # TODO: pass handle through method
         entity = get_entity(handle)
-        return entity.get_transform()
+        # new_position = Vector3(2, 2, 2)
+        entity.set_position(1.0, -4.8, -6.0)
+        return entity.get_transform().position.x
 
 
 PythonScript()
--- crates/dream-app/src/python_script_component_system.rs
+++ crates/dream-app/src/python_script_component_system.rs
@@ -10,7 +10,6 @@ use rustpython_vm::{
     TryFromBorrowedObject, VirtualMachine,
 };
 
-use dream_ecs::component::Transform;
 use dream_ecs::entity::Entity;
 use dream_ecs::scene::Scene;
 
@@ -42,7 +41,7 @@ impl System for PythonScriptComponentSystem {
             .expect("Unable to upgrade")
             .lock()
             .expect("Unable to lock")
-            .get_entities_with_component::<Transform>();
+            .get_entities_with_component::<dream_ecs::component::Transform>();
         for entity_id in transform_entities {
             let script = include_str!("default-files/script.py");
             // let _entity = Entity::from_handle(entity_id, scene);
@@ -63,7 +62,7 @@ impl System for PythonScriptComponentSystem {
                 vm.run_code_obj(code_obj, scope)
                     .map(|value| {
                         let update = value.get_attr("update", vm).unwrap();
-                        let handle = vm.ctx.new_int(entity_id as i32).into();
+                        let handle = vm.ctx.new_int(entity_id).into();
                         let args = vec![handle];
                         let res = vm
                             .invoke(&update, args)
@@ -108,16 +107,22 @@ mod dream {
         }
 
         #[pymethod]
-        fn print_in_rust_from_python(&self) {
-            println!("Calling a rust method from python");
+        fn get_transform(&self) -> Transform {
+            let scene = SCENE.lock().unwrap().as_ref().unwrap().clone();
+            let entity = Entity::from_handle(self.handle, scene);
+            let transform: Option<dream_ecs::component::Transform> = entity.get_component();
+            transform.expect("No transform component").into()
         }
 
         #[pymethod]
-        fn get_transform(&self) -> f32 {
+        fn set_position(&self, x: f32, y: f32, z: f32) {
+            let position = Vector3 { x, y, z };
             let scene = SCENE.lock().unwrap().as_ref().unwrap().clone();
             let entity = Entity::from_handle(self.handle, scene);
-            let transform: Option<Transform> = entity.get_component();
-            transform.expect("No transform component").position.x
+            let transform: Option<dream_ecs::component::Transform> = entity.get_component();
+            let mut transform = transform.unwrap();
+            transform.position = position.into();
+            entity.add_component(transform);
         }
     }
 
@@ -125,6 +130,94 @@ mod dream {
         fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
             let handle = obj.get_attr("handle", vm)?.try_into_value::<u64>(vm)?;
             Ok(PythonEntity { handle })
+        }
+    }
+
+    #[pyattr]
+    #[pyclass(module = "dream", name = "Transform")]
+    #[derive(Debug, PyPayload)]
+    struct Transform {
+        position: Vector3,
+    }
+
+    #[pyclass]
+    impl Transform {
+        #[pygetset]
+        fn position(&self) -> Vector3 {
+            self.position
+        }
+    }
+
+    impl Into<Transform> for dream_ecs::component::Transform {
+        fn into(self) -> Transform {
+            Transform {
+                position: self.position.into(),
+            }
+        }
+    }
+
+    impl TryFromBorrowedObject for Transform {
+        fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
+            let position = obj
+                .get_attr("position", vm)?
+                .try_into_value::<Vector3>(vm)?;
+            Ok(Transform { position })
+        }
+    }
+
+    #[pyattr]
+    #[pyclass(module = "dream", name = "Vector3")]
+    #[derive(Debug, Clone, Copy, PyPayload)]
+    struct Vector3 {
+        x: f32,
+        y: f32,
+        z: f32,
+    }
+
+    #[pyclass]
+    impl Vector3 {
+        #[pygetset]
+        fn x(&self) -> f32 {
+            self.x
+        }
+
+        #[pygetset]
+        fn y(&self) -> f32 {
+            self.y
+        }
+
+        #[pygetset]
+        fn z(&self) -> f32 {
+            self.z
+        }
+    }
+
+    impl Into<Vector3> for dream_math::Vector3 {
+        fn into(self) -> Vector3 {
+            Vector3 {
+                x: self.x,
+                y: self.y,
+                z: self.z,
+            }
+        }
+    }
+
+    impl Into<dream_math::Vector3> for Vector3 {
+        fn into(self) -> dream_math::Vector3 {
+            dream_math::Vector3 {
+                x: self.x,
+                y: self.y,
+                z: self.z,
+            }
+        }
+    }
+
+    impl TryFromBorrowedObject for Vector3 {
+        fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
+            let x = obj.get_attr("x", vm)?.try_into_value::<f32>(vm)?;
+            let y = obj.get_attr("y", vm)?.try_into_value::<f32>(vm)?;
+            let z = obj.get_attr("z", vm)?.try_into_value::<f32>(vm)?;
+            Ok(Vector3 { x, y, z })
         }
     }
 }
