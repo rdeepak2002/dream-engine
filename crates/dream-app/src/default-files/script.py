bcb70297d15f4ef3c39221cce156eb3a66cbe4ef
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-08-16 14:06:01 -0700

    Rename bridge classes to internal
    Took 48 minutes

--- crates/dream-app/src/default-files/script.py
+++ crates/dream-app/src/default-files/script.py
@@ -1,44 +1,21 @@
-from dream_py import Entity, Vector3, fix_vec_3
+from dream_py import vec3
+
+
+# class Vector3:
 
 
 class SampleScript:
     def __init__(self):
         self.x: float = -2.5
 
-    def update(self, dt: float, entity: Entity):
-        # self.x += 2.0 * dt
-        # if self.x >= 2.5:
-        #     self.x = -2.5
-        # entity.set_position(self.x, -4.8, -6.0)
+    def bound_x(self, x):
+        return x if x < 2.5 else -2.5
 
-        # new_position = Vector3(1.0, 2.0, 3.0)
-        # new_position = fix_vec_3(new_position)
-        # entity.set_position(new_position.x, new_position.y, new_position.z)
-
-        # new_position = entity.get_transform().position
-        # new_position = new_position + Vector3(2.0 * dt, 0.0, 0.0)
-        # entity.set_position(new_position.x, new_position.y, new_position.z)
-
-        current_position: Vector3 = entity.get_transform().get_position()
-        tmp = Vector3(2.0 * dt, 0.0, 0.0)
-        tmp.x = 2.0 * dt
-        tmp.y = 0.0
-        tmp.z = 0.0
-        velocity: Vector3 = fix_vec_3(tmp)
-
-        x_new: float = current_position.get_x() + velocity.get_x()
-        y_new: float = current_position.get_y() + velocity.get_y()
-        z_new: float = current_position.get_z() + velocity.get_z()
-
-        if x_new >= 2.5:
-            x_new = -2.5
-
-        tmp = Vector3(x_new, y_new, z_new)
-        tmp.x = x_new
-        tmp.y = y_new
-        tmp.z = z_new
-        new_position: Vector3 = fix_vec_3(tmp)
-        entity.set_position(new_position.get_x(), new_position.get_y(), new_position.get_z())
+    def update(self, dt: float, entity):
+        cur_pos = entity.get_position()
+        vel = vec3(2.0 * dt, 0.0, 0.0)
+        new_pos = vec3(self.bound_x(cur_pos.x + vel.x), cur_pos.y + vel.y, cur_pos.z + vel.z)
+        entity.set_position(new_pos)
 
 
 SampleScript()
--- crates/dream-app/src/python_script_component_system.rs
+++ crates/dream-app/src/python_script_component_system.rs
@@ -79,7 +79,7 @@ impl System for PythonScriptComponentSystem {
 
                 if let Ok(update) = entity_script.get_attr("update", vm) {
                     // let entity = dream_py::Entity { handle: entity_id }.to_pyobject(vm);
-                    let entity = dream_py::Entity { handle: entity_id }.to_pyobject(vm);
+                    let entity = dream_py::EntityInternal { handle: entity_id }.to_pyobject(vm);
                     let args = vec![vm.ctx.new_float(dt as f64).into(), entity];
                     let res = update.call(args, vm);
                     if let Err(..) = res {
@@ -130,24 +130,29 @@ pub(crate) mod dream_py {
     use super::*;
 
     #[pyfunction]
-    fn get_entity(handle: u64, _vm: &VirtualMachine) -> PyResult<Entity> {
-        Ok(Entity { handle })
+    fn get_entity(handle: u64, _vm: &VirtualMachine) -> PyResult<EntityInternal> {
+        Ok(EntityInternal { handle })
     }
 
     #[pyfunction]
-    fn fix_vec_3(obj: PyObjectRef, vm: &VirtualMachine) -> PyResult<Vector3> {
-        Vector3::try_from_borrowed_object(vm, &obj.to_pyobject(vm))
+    fn vec3(x: f64, y: f64, z: f64, _vm: &VirtualMachine) -> PyResult<Vector3Internal> {
+        Ok(Vector3Internal { x, y, z })
     }
 
+    // #[pyfunction]
+    // fn fix_vec_3(obj: PyObjectRef, vm: &VirtualMachine) -> PyResult<Vector3> {
+    //     Vector3::try_from_borrowed_object(vm, &obj.to_pyobject(vm))
+    // }
+
     #[pyattr]
-    #[pyclass(module = "dream_py", name = "Entity")]
+    #[pyclass(module = "dream_py", name = "EntityInternal")]
     #[derive(Debug, PyPayload)]
-    pub(crate) struct Entity {
+    pub(crate) struct EntityInternal {
         pub(crate) handle: u64,
     }
 
     #[pyclass]
-    impl Entity {
+    impl EntityInternal {
         // #[pygetset]
         // fn handle(&self) -> u64 {
         //     self.handle
@@ -166,10 +171,33 @@ pub(crate) mod dream_py {
             Ok(Transform::from(transform.expect("No transform component")))
         }
 
+        #[inline]
         #[pymethod]
-        fn set_position(&self, x: f64, y: f64, z: f64) {
+        fn get_position(&self) -> PyResult<Vector3Internal> {
+            let scene = SCENE.lock().unwrap().as_ref().unwrap().clone();
+            let entity = dream_ecs::entity::Entity::from_handle(self.handle, scene);
+            let transform: Option<dream_ecs::component::Transform> = entity.get_component();
+            let transform = transform.unwrap();
+            Ok(Vector3Internal::from(transform.position))
+        }
+
+        // #[pymethod]
+        // fn set_position(&self, x: f64, y: f64, z: f64) {
+        //     // println!("Setting position to ({x}, {y}, {z})");
+        //     let position = Vector3 { x, y, z };
+        //     let scene = SCENE.lock().unwrap().as_ref().unwrap().clone();
+        //     let entity = dream_ecs::entity::Entity::from_handle(self.handle, scene);
+        //     let transform: Option<dream_ecs::component::Transform> = entity.get_component();
+        //     let mut transform = transform.unwrap();
+        //     transform.position = dream_math::Vector3::from(position);
+        //     entity.add_component(transform);
+        // }
+
+        #[inline]
+        #[pymethod]
+        fn set_position(&self, position: Vector3Internal) {
             // println!("Setting position to ({x}, {y}, {z})");
-            let position = Vector3 { x, y, z };
+            // let position = Vector3 { x, y, z };
             let scene = SCENE.lock().unwrap().as_ref().unwrap().clone();
             let entity = dream_ecs::entity::Entity::from_handle(self.handle, scene);
             let transform: Option<dream_ecs::component::Transform> = entity.get_component();
@@ -179,10 +207,10 @@ pub(crate) mod dream_py {
         }
     }
 
-    impl TryFromBorrowedObject<'_> for Entity {
+    impl TryFromBorrowedObject<'_> for EntityInternal {
         fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
             let handle = obj.get_attr("handle", vm)?.try_into_value::<u64>(vm)?;
-            Ok(Entity { handle })
+            Ok(EntityInternal { handle })
         }
     }
 
@@ -190,7 +218,7 @@ pub(crate) mod dream_py {
     #[pyclass(module = "dream_py", name = "Transform")]
     #[derive(Debug, PyPayload)]
     struct Transform {
-        position: Vector3,
+        position: Vector3Internal,
     }
 
     #[pyclass]
@@ -200,16 +228,16 @@ pub(crate) mod dream_py {
         //     Ok(self.position)
         // }
 
-        #[pymethod]
-        fn get_position(&self) -> PyResult<Vector3> {
-            Ok(self.position)
-        }
+        // #[pymethod]
+        // fn get_position(&self) -> PyResult<Vector3> {
+        //     Ok(self.position)
+        // }
     }
 
     impl From<dream_ecs::component::Transform> for Transform {
         fn from(transform: dream_ecs::component::Transform) -> Self {
             Transform {
-                position: Vector3::from(transform.position),
+                position: Vector3Internal::from(transform.position),
             }
         }
     }
@@ -218,50 +246,55 @@ pub(crate) mod dream_py {
         fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
             let position = obj
                 .get_attr("position", vm)?
-                .try_into_value::<Vector3>(vm)?;
+                .try_into_value::<Vector3Internal>(vm)?;
             Ok(Transform { position })
         }
     }
 
     #[pyattr]
-    #[pyclass(module = "dream_py", name = "Vector3")]
-    #[derive(Debug, Clone, Copy, PyPayload)]
-    struct Vector3 {
+    #[pyclass(module = "dream_py", name = "Vector3Internal")]
+    #[derive(Debug, PyPayload)]
+    struct Vector3Internal {
         x: f64,
         y: f64,
         z: f64,
     }
 
     #[pyclass]
-    impl Vector3 {
-        // #[pygetset]
-        // fn x(&self) -> f64 {
+    impl Vector3Internal {
+        #[pygetset]
+        fn x(&self) -> f64 {
+            self.x.clone()
+        }
+
+        #[pygetset]
+        fn y(&self) -> f64 {
+            self.y.clone()
+        }
+
+        #[pygetset]
+        fn z(&self) -> f64 {
+            self.z.clone()
+        }
+
+        // #[pymethod]
+        // fn get_x(&self) -> f64 {
         //     self.x
         // }
         //
-        // #[pygetset]
-        // fn y(&self) -> f64 {
+        // #[pymethod]
+        // fn get_y(&self) -> f64 {
         //     self.y
         // }
         //
-        // #[pygetset]
-        // fn z(&self) -> f64 {
+        // #[pymethod]
+        // fn get_z(&self) -> f64 {
         //     self.z
         // }
 
         #[pymethod]
-        fn get_x(&self) -> f64 {
-            self.x
-        }
-
-        #[pymethod]
-        fn get_y(&self) -> f64 {
-            self.y
-        }
-
-        #[pymethod]
-        fn get_z(&self) -> f64 {
-            self.z
+        fn print_in_rust_from_python(&self) {
+            println!("Calling a rust method from python");
         }
 
         // #[pymethod]
@@ -278,58 +311,62 @@ pub(crate) mod dream_py {
         // fn set_z(&mut self, z: f64) {
         //     self.z = z;
         // }
-
-        pub(super) const AS_NUMBER: PyNumberMethods = PyNumberMethods {
-            add: Some(|a, b, vm| {
-                let a = Vector3::try_from_borrowed_object(vm, a).unwrap();
-                let b = Vector3::try_from_borrowed_object(vm, b).unwrap();
-                let res: Self =
-                    (dream_math::Vector3::from(a) + dream_math::Vector3::from(b)).into();
-                res.to_pyresult(vm)
-            }),
-            subtract: Some(|a, b, vm| {
-                let a = Vector3::try_from_borrowed_object(vm, a).unwrap();
-                let b = Vector3::try_from_borrowed_object(vm, b).unwrap();
-                let res: Self =
-                    (dream_math::Vector3::from(a) - dream_math::Vector3::from(b)).into();
-                res.to_pyresult(vm)
-            }),
-            multiply: Some(|a, b, vm| {
-                // scenario where a is a scalar
-                if let a = a.try_float(vm) {
-                    let a = a.unwrap().to_f64() as f32;
-                    let b = Vector3::try_from_borrowed_object(vm, b).unwrap();
-                    let res: Self = (a * dream_math::Vector3::from(b)).into();
-                    return res.to_pyresult(vm);
-                }
-
-                // scenario where b is a scalar
-                if let b = b.try_float(vm) {
-                    let a = Vector3::try_from_borrowed_object(vm, a).unwrap();
-                    let b = b.unwrap().to_f64() as f32;
-                    let res: Self = (dream_math::Vector3::from(a) * b).into();
-                    return res.to_pyresult(vm);
-                }
-
-                // scenario where both a and b are vectors
-                let a = Vector3::try_from_borrowed_object(vm, a).unwrap();
-                let b = Vector3::try_from_borrowed_object(vm, b).unwrap();
-                let res: Self =
-                    (dream_math::Vector3::from(a) * dream_math::Vector3::from(b)).into();
-                res.to_pyresult(vm)
-            }),
-            // power: Some(|a, b, c, vm| {...}),
-            // negative: Some(|num, vm| (&PyInt::number_downcast(num).value).neg().to_pyresult(vm)),
-            // positive: Some(|num, vm| Ok(PyInt::number_downcast_exact(num, vm).into())),
-            // absolute: Some(|num, vm| PyInt::number_downcast(num).value.abs().to_pyresult(vm)),
-            // invert: Some(|num, vm| (&PyInt::number_downcast(num).value).not().to_pyresult(vm)),
-            // floor_divide: Some(|a, b, vm| PyInt::number_op(a, b, inner_floordiv, vm)),
-            // true_divide: Some(|a, b, vm| PyInt::number_op(a, b, inner_truediv, vm)),
-            ..PyNumberMethods::NOT_IMPLEMENTED
-        };
     }
 
-    impl Constructor for Vector3 {
+    impl AsNumber for Vector3Internal {
+        fn as_number() -> &'static PyNumberMethods {
+            &PyNumberMethods {
+                add: Some(|a, b, vm| {
+                    let a = Vector3Internal::try_from_borrowed_object(vm, a).unwrap();
+                    let b = Vector3Internal::try_from_borrowed_object(vm, b).unwrap();
+                    let res: Self =
+                        (dream_math::Vector3::from(a) + dream_math::Vector3::from(b)).into();
+                    res.to_pyresult(vm)
+                }),
+                subtract: Some(|a, b, vm| {
+                    let a = Vector3Internal::try_from_borrowed_object(vm, a).unwrap();
+                    let b = Vector3Internal::try_from_borrowed_object(vm, b).unwrap();
+                    let res: Self =
+                        (dream_math::Vector3::from(a) - dream_math::Vector3::from(b)).into();
+                    res.to_pyresult(vm)
+                }),
+                multiply: Some(|a, b, vm| {
+                    // scenario where a is a scalar
+                    if let a = a.try_float(vm) {
+                        let a = a.unwrap().to_f64() as f32;
+                        let b = Vector3Internal::try_from_borrowed_object(vm, b).unwrap();
+                        let res: Self = (a * dream_math::Vector3::from(b)).into();
+                        return res.to_pyresult(vm);
+                    }
+
+                    // scenario where b is a scalar
+                    if let b = b.try_float(vm) {
+                        let a = Vector3Internal::try_from_borrowed_object(vm, a).unwrap();
+                        let b = b.unwrap().to_f64() as f32;
+                        let res: Self = (dream_math::Vector3::from(a) * b).into();
+                        return res.to_pyresult(vm);
+                    }
+
+                    // scenario where both a and b are vectors
+                    let a = Vector3Internal::try_from_borrowed_object(vm, a).unwrap();
+                    let b = Vector3Internal::try_from_borrowed_object(vm, b).unwrap();
+                    let res: Self =
+                        (dream_math::Vector3::from(a) * dream_math::Vector3::from(b)).into();
+                    res.to_pyresult(vm)
+                }),
+                // power: Some(|a, b, c, vm| {...}),
+                // negative: Some(|num, vm| (&PyInt::number_downcast(num).value).neg().to_pyresult(vm)),
+                // positive: Some(|num, vm| Ok(PyInt::number_downcast_exact(num, vm).into())),
+                // absolute: Some(|num, vm| PyInt::number_downcast(num).value.abs().to_pyresult(vm)),
+                // invert: Some(|num, vm| (&PyInt::number_downcast(num).value).not().to_pyresult(vm)),
+                // floor_divide: Some(|a, b, vm| PyInt::number_op(a, b, inner_floordiv, vm)),
+                // true_divide: Some(|a, b, vm| PyInt::number_op(a, b, inner_truediv, vm)),
+                ..PyNumberMethods::NOT_IMPLEMENTED
+            }
+        }
+    }
+
+    impl Constructor for Vector3Internal {
         type Args = (f64, f64, f64);
 
         fn py_new(_cls: PyTypeRef, args: Self::Args, vm: &VirtualMachine) -> PyResult {
@@ -342,14 +379,8 @@ pub(crate) mod dream_py {
         }
     }
 
-    // impl AsNumber for Vector3 {
-    //     fn as_number() -> &'static PyNumberMethods {
-    //         todo!()
-    //     }
-    // }
-
-    impl From<Vector3> for dream_math::Vector3 {
-        fn from(vec3: Vector3) -> Self {
+    impl From<Vector3Internal> for dream_math::Vector3 {
+        fn from(vec3: Vector3Internal) -> Self {
             dream_math::Vector3 {
                 x: vec3.x as f32,
                 y: vec3.y as f32,
@@ -358,9 +389,9 @@ pub(crate) mod dream_py {
         }
     }
 
-    impl From<dream_math::Vector3> for Vector3 {
+    impl From<dream_math::Vector3> for Vector3Internal {
         fn from(vec3: dream_math::Vector3) -> Self {
-            Vector3 {
+            Vector3Internal {
                 x: vec3.x as f64,
                 y: vec3.y as f64,
                 z: vec3.z as f64,
@@ -368,7 +399,7 @@ pub(crate) mod dream_py {
         }
     }
 
-    impl TryFromBorrowedObject<'_> for Vector3 {
+    impl TryFromBorrowedObject<'_> for Vector3Internal {
         fn try_from_borrowed_object(vm: &VirtualMachine, obj: &PyObject) -> PyResult<Self> {
             // TODO: get from object
             // let x = 1.0;
@@ -394,7 +425,7 @@ pub(crate) mod dream_py {
                 .try_float(vm)
                 .expect("Unable to convert z to float")
                 .to_f64();
-            Ok(Vector3 { x, y, z })
+            Ok(Vector3Internal { x, y, z })
         }
     }
 }
