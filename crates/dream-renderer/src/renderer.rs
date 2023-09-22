020380494f42e3d9c8b6c2daf1c819860bc168ac
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-09-22 00:57:59 -0400

    Rename forward rendering associated render pipeline and render pass to reduce ambiguity
    Took 5 minutes

--- crates/dream-renderer/src/renderer.rs
+++ crates/dream-renderer/src/renderer.rs
@@ -57,7 +57,7 @@ pub struct RendererWgpu {
     camera_uniform: CameraUniform,
     camera_buffer: wgpu::Buffer,
     render_pipeline_write_g_buffers: wgpu::RenderPipeline,
-    render_pipeline: wgpu::RenderPipeline,
+    render_pipeline_forward_rendering: wgpu::RenderPipeline,
     depth_texture: texture::Texture,
     frame_texture: texture::Texture,
     camera_bind_group: wgpu::BindGroup,
@@ -493,65 +493,66 @@ impl RendererWgpu {
                 multiview: None,
             });
 
-        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&render_pipeline_layout),
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main",
-                buffers: &[ModelVertex::desc(), InstanceRaw::desc()],
-                // buffers: &[Vertex::desc()],
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState {
-                        color: wgpu::BlendComponent {
-                            src_factor: wgpu::BlendFactor::SrcAlpha,
-                            dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
-                            operation: wgpu::BlendOperation::Add,
-                        },
-                        alpha: wgpu::BlendComponent::OVER,
-                    }),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-            }),
-            primitive: wgpu::PrimitiveState {
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
-                // or Features::POLYGON_MODE_POINT
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            depth_stencil: Some(wgpu::DepthStencilState {
-                format: texture::Texture::DEPTH_FORMAT,
-                depth_write_enabled: true,
-                depth_compare: wgpu::CompareFunction::Less,
-                stencil: wgpu::StencilState::default(),
-                bias: wgpu::DepthBiasState::default(),
-            }),
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview: None,
-        });
+        let render_pipeline_forward_rendering =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline Forward Rendering"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main",
+                    buffers: &[ModelVertex::desc(), InstanceRaw::desc()],
+                    // buffers: &[Vertex::desc()],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main",
+                    targets: &[Some(wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState {
+                            color: wgpu::BlendComponent {
+                                src_factor: wgpu::BlendFactor::SrcAlpha,
+                                dst_factor: wgpu::BlendFactor::OneMinusSrcAlpha,
+                                operation: wgpu::BlendOperation::Add,
+                            },
+                            alpha: wgpu::BlendComponent::OVER,
+                        }),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    })],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::POLYGON_MODE_LINE
+                    // or Features::POLYGON_MODE_POINT
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLIP_CONTROL
+                    unclipped_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: Some(wgpu::DepthStencilState {
+                    format: texture::Texture::DEPTH_FORMAT,
+                    depth_write_enabled: true,
+                    depth_compare: wgpu::CompareFunction::Less,
+                    stencil: wgpu::StencilState::default(),
+                    bias: wgpu::DepthBiasState::default(),
+                }),
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+                multiview: None,
+            });
 
         Self {
             surface,
             device,
             queue,
             config,
-            render_pipeline,
+            render_pipeline_forward_rendering,
             depth_texture,
             frame_texture,
             frame_texture_view: None,
@@ -908,34 +909,35 @@ impl RendererWgpu {
         // render final result
         {
             // define render pass
-            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &texture_view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
+            let mut render_pass_forward_rendering =
+                encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: Some("Render Pass Forward Rendering"),
+                    color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                        view: &texture_view,
+                        resolve_target: None,
+                        ops: wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(wgpu::Color {
+                                r: 0.1,
+                                g: 0.2,
+                                b: 0.3,
+                                a: 1.0,
+                            }),
+                            store: true,
+                        },
+                    })],
+                    depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
+                        view: &self.depth_texture.view,
+                        depth_ops: Some(wgpu::Operations {
+                            load: wgpu::LoadOp::Clear(1.0),
+                            store: true,
                         }),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                    view: &self.depth_texture.view,
-                    depth_ops: Some(wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(1.0),
-                        store: true,
+                        stencil_ops: None,
                     }),
-                    stencil_ops: None,
-                }),
-            });
-            render_pass.set_pipeline(&self.render_pipeline);
+                });
+            render_pass_forward_rendering.set_pipeline(&self.render_pipeline_forward_rendering);
 
             // camera bind group
-            render_pass.set_bind_group(0, &self.camera_bind_group, &[]);
+            render_pass_forward_rendering.set_bind_group(0, &self.camera_bind_group, &[]);
 
             // iterate through all meshes that should be instanced drawn
             for (render_map_key, transforms) in &self.render_map {
@@ -961,21 +963,26 @@ impl RendererWgpu {
                     .instance_buffer_map
                     .get(render_map_key)
                     .expect("No instance buffer found in map");
-                render_pass.set_vertex_buffer(1, instance_buffer.slice(..));
+                render_pass_forward_rendering.set_vertex_buffer(1, instance_buffer.slice(..));
                 // get the material and set it in the bind group
                 let material = model
                     .materials
                     .get(mesh.material)
                     .expect("No material at index");
                 if material.pbr_material_textures_bind_group.is_some() {
-                    render_pass.set_bind_group(1, &material.pbr_material_factors_bind_group, &[]);
-                    render_pass.set_bind_group(
+                    render_pass_forward_rendering.set_bind_group(
+                        1,
+                        &material.pbr_material_factors_bind_group,
+                        &[],
+                    );
+                    render_pass_forward_rendering.set_bind_group(
                         2,
                         material.pbr_material_textures_bind_group.as_ref().unwrap(),
                         &[],
                     );
                     // draw the mesh
-                    render_pass.draw_mesh_instanced(mesh, 0..transforms.len() as u32);
+                    render_pass_forward_rendering
+                        .draw_mesh_instanced(mesh, 0..transforms.len() as u32);
                 }
             }
         }
