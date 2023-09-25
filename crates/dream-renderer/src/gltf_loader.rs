baef464387f14fb3c50178abb92abd4be8aabd0f
Deepak Ramalingam <rdeepak2002@gmail.com>
2023-09-24 20:15:28 -0400

    Make it easier to enable usage of mikktspace algorithm
    Took 41 minutes

--- crates/dream-renderer/src/gltf_loader.rs
+++ crates/dream-renderer/src/gltf_loader.rs
@@ -90,19 +90,20 @@ fn process_gltf_child_node<'a>(child_node: gltf::Node<'a>, mesh_list: &mut Vec<M
 // not being used due to how long it takes to run
 pub type Face = [u32; 3];
 
-struct MkktSpaceMesh {
-    faces: Vec<Face>,
+struct MeshVerticesAndIndicesContainer {
+    indices: Vec<u32>,
     vertices: Vec<ModelVertex>,
 }
 
-fn vertex(mesh: &MkktSpaceMesh, face: usize, vert: usize) -> &ModelVertex {
-    let vs: &[u32; 3] = &mesh.faces[face];
-    &mesh.vertices[vs[vert] as usize]
+impl MeshVerticesAndIndicesContainer {
+    fn vertex(&self, face: usize, vert: usize) -> &ModelVertex {
+        &self.vertices[self.indices[face * 3 + vert] as usize]
+    }
 }
 
-impl mikktspace::Geometry for MkktSpaceMesh {
+impl mikktspace::Geometry for MeshVerticesAndIndicesContainer {
     fn num_faces(&self) -> usize {
-        self.faces.len()
+        self.indices.len() / 3
     }
 
     fn num_vertices_of_face(&self, _face: usize) -> usize {
@@ -110,20 +111,19 @@ impl mikktspace::Geometry for MkktSpaceMesh {
     }
 
     fn position(&self, face: usize, vert: usize) -> [f32; 3] {
-        vertex(self, face, vert).position.into()
+        self.vertex(face, vert).position
     }
 
     fn normal(&self, face: usize, vert: usize) -> [f32; 3] {
-        vertex(self, face, vert).normal.into()
+        self.vertex(face, vert).normal
     }
 
     fn tex_coord(&self, face: usize, vert: usize) -> [f32; 2] {
-        vertex(self, face, vert).tex_coords.into()
+        self.vertex(face, vert).tex_coords
     }
 
     fn set_tangent_encoded(&mut self, tangent: [f32; 4], face: usize, vert: usize) {
-        let vs: Face = self.faces[face];
-        self.vertices[vs[vert] as usize].tangent = tangent;
+        self.vertices[self.indices[face * 3 + vert] as usize].tangent = tangent;
     }
 }
 
@@ -135,24 +135,29 @@ fn get_dream_meshes_from_gltf_mesh(
     let mut meshes = Vec::new();
     let primitives = mesh.primitives();
     primitives.for_each(|primitive| {
+        let mut mesh_vertices_and_indices = MeshVerticesAndIndicesContainer {
+            vertices: Vec::new(),
+            indices: Vec::new(),
+        };
+
         let reader = primitive.reader(|buffer| Some(&buffer_data[buffer.index()]));
-        let mut vertices = Vec::new();
         if let Some(vertex_attribute) = reader.read_positions() {
             vertex_attribute.for_each(|vertex| {
-                // dbg!(vertex);
-                vertices.push(crate::model::ModelVertex {
-                    position: vertex,
-                    tex_coords: Default::default(),
-                    normal: Default::default(),
-                    tangent: [0.0, 0.0, 0.0, 0.0],
-                })
+                mesh_vertices_and_indices
+                    .vertices
+                    .push(crate::model::ModelVertex {
+                        position: vertex,
+                        tex_coords: Default::default(),
+                        normal: Default::default(),
+                        tangent: [0.0, 0.0, 0.0, 0.0],
+                    })
             });
         }
         let mut manually_compute_tangents = false;
         if let Some(tangent_attribute) = reader.read_tangents() {
             let mut tangent_index = 0;
             tangent_attribute.for_each(|tangent| {
-                vertices[tangent_index].tangent = tangent;
+                mesh_vertices_and_indices.vertices[tangent_index].tangent = tangent;
 
                 tangent_index += 1;
             });
@@ -162,8 +167,7 @@ fn get_dream_meshes_from_gltf_mesh(
         if let Some(normal_attribute) = reader.read_normals() {
             let mut normal_index = 0;
             normal_attribute.for_each(|normal| {
-                // dbg!(normal);
-                vertices[normal_index].normal = normal;
+                mesh_vertices_and_indices.vertices[normal_index].normal = normal;
 
                 normal_index += 1;
             });
@@ -171,75 +175,77 @@ fn get_dream_meshes_from_gltf_mesh(
         if let Some(tex_coord_attribute) = reader.read_tex_coords(0).map(|v| v.into_f32()) {
             let mut tex_coord_index = 0;
             tex_coord_attribute.for_each(|tex_coord| {
-                // dbg!(tex_coord);
-                vertices[tex_coord_index].tex_coords = tex_coord;
+                mesh_vertices_and_indices.vertices[tex_coord_index].tex_coords = tex_coord;
 
                 tex_coord_index += 1;
             });
         }
 
-        let mut indices = Vec::new();
         if let Some(indices_raw) = reader.read_indices() {
-            // dbg!(indices_raw);
-            indices.append(&mut indices_raw.into_u32().collect::<Vec<u32>>());
+            for idx in indices_raw.into_u32() {
+                mesh_vertices_and_indices.indices.push(idx);
+            }
         }
 
+        let use_mikktspace_algorithm = false;
+
         if manually_compute_tangents {
-            // commented-out example of how to use MikkTSpace algorithm
-            // let mut faces = Vec::<Face>::new();
-            // for i in (0..indices.len()).step_by(3) {
-            //     faces.push([indices[i], indices[i + 1], indices[i + 2]]);
-            // }
-            // let mut m = MkktSpaceMesh {
-            //     vertices: vertices.clone(),
-            //     faces: faces.clone(),
-            // };
-            // let ret = mikktspace::generate_tangents(&mut m);
-            // assert!(ret);
-            // vertices = m.vertices;
-            for i in (0..indices.len()).step_by(3) {
-                let v0 = vertices[indices[i] as usize];
-                let v1 = vertices[indices[i + 1] as usize];
-                let v2 = vertices[indices[i + 2] as usize];
+            if use_mikktspace_algorithm {
+                let ret = mikktspace::generate_tangents(&mut mesh_vertices_and_indices);
+            } else {
+                for i in (0..mesh_vertices_and_indices.indices.len()).step_by(3) {
+                    let v0 = mesh_vertices_and_indices.vertices
+                        [mesh_vertices_and_indices.indices[i] as usize];
+                    let v1 = mesh_vertices_and_indices.vertices
+                        [mesh_vertices_and_indices.indices[i + 1] as usize];
+                    let v2 = mesh_vertices_and_indices.vertices
+                        [mesh_vertices_and_indices.indices[i + 2] as usize];
 
-                let edge1 = Vector3::from(v1.position) - Vector3::from(v0.position);
-                let edge2 = Vector3::from(v2.position) - Vector3::from(v0.position);
+                    let edge1 = Vector3::from(v1.position) - Vector3::from(v0.position);
+                    let edge2 = Vector3::from(v2.position) - Vector3::from(v0.position);
 
-                let delta_u1 = v1.tex_coords[0] - v0.tex_coords[0];
-                let delta_v1 = v1.tex_coords[1] - v0.tex_coords[1];
-                let delta_u2 = v2.tex_coords[0] - v0.tex_coords[0];
-                let delta_v2 = v2.tex_coords[1] - v0.tex_coords[1];
+                    let delta_u1 = v1.tex_coords[0] - v0.tex_coords[0];
+                    let delta_v1 = v1.tex_coords[1] - v0.tex_coords[1];
+                    let delta_u2 = v2.tex_coords[0] - v0.tex_coords[0];
+                    let delta_v2 = v2.tex_coords[1] - v0.tex_coords[1];
 
-                let f = 1.0 / (delta_u1 * delta_v2 - delta_u2 * delta_v1);
+                    let f = 1.0 / (delta_u1 * delta_v2 - delta_u2 * delta_v1);
 
-                let mut tangent = Vector3::new(0., 0., 0.);
+                    let mut tangent = Vector3::new(0., 0., 0.);
 
-                tangent.x = f * (delta_v2 * edge1.x - delta_v1 * edge2.x);
-                tangent.y = f * (delta_v2 * edge1.y - delta_v1 * edge2.y);
-                tangent.z = f * (delta_v2 * edge1.z - delta_v1 * edge2.z);
+                    tangent.x = f * (delta_v2 * edge1.x - delta_v1 * edge2.x);
+                    tangent.y = f * (delta_v2 * edge1.y - delta_v1 * edge2.y);
+                    tangent.z = f * (delta_v2 * edge1.z - delta_v1 * edge2.z);
 
-                vertices[indices[i] as usize].tangent = [tangent.x, tangent.y, tangent.z, 1.0];
-                vertices[indices[i + 1] as usize].tangent = [tangent.x, tangent.y, tangent.z, 1.0];
-                vertices[indices[i + 2] as usize].tangent = [tangent.x, tangent.y, tangent.z, 1.0];
+                    mesh_vertices_and_indices.vertices
+                        [mesh_vertices_and_indices.indices[i] as usize]
+                        .tangent = [tangent.x, tangent.y, tangent.z, 1.0];
+                    mesh_vertices_and_indices.vertices
+                        [mesh_vertices_and_indices.indices[i + 1] as usize]
+                        .tangent = [tangent.x, tangent.y, tangent.z, 1.0];
+                    mesh_vertices_and_indices.vertices
+                        [mesh_vertices_and_indices.indices[i + 2] as usize]
+                        .tangent = [tangent.x, tangent.y, tangent.z, 1.0];
+                }
             }
         }
 
         let mesh_name = mesh.name().expect("No mesh name found");
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some(&format!("{mesh_name} Vertex Buffer")),
-            contents: bytemuck::cast_slice(&vertices),
+            contents: bytemuck::cast_slice(&mesh_vertices_and_indices.vertices),
             usage: wgpu::BufferUsages::VERTEX,
         });
         let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some(&format!("{mesh_name} Index Buffer")),
-            contents: bytemuck::cast_slice(&indices),
+            contents: bytemuck::cast_slice(&mesh_vertices_and_indices.indices),
             usage: wgpu::BufferUsages::INDEX,
         });
         meshes.push(crate::model::Mesh {
             name: mesh_name.to_string(),
             vertex_buffer,
             index_buffer,
-            num_elements: indices.len() as u32,
+            num_elements: mesh_vertices_and_indices.indices.len() as u32,
             material: primitive.material().index().unwrap_or(0),
         });
     });
