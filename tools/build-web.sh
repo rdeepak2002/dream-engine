8a7cb4159dc6d6ccda5ef81e55668dd16eea6443
Deepak Ramalingam <rdeepak2002@gmail.com>
2024-01-11 19:35:16 -0800

    Update toolchain used for web build to be 2024 nightly
    Took 8 minutes

--- tools/build-web.sh
+++ tools/build-web.sh
@@ -1,5 +1,5 @@
-rustup run nightly-2023-06-10 wasm-pack build --target web --out-dir ../../web/build --release
-rustup run nightly-2023-06-10 wasm-pack build --target web --out-dir ../../web/build-webgl --release --features=wgpu/webgl
+rustup run nightly-2024-01-11 wasm-pack build --target web --out-dir ../../web/build --release
+rustup run nightly-2024-01-11 wasm-pack build --target web --out-dir ../../web/build-webgl --release --features=wgpu/webgl
 
 # start server is build succeeded
 if [ $? -eq 0 ]; then
